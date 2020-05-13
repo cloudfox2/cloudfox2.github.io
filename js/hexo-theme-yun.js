fb62864904fa60186397a090d889cd459e44078e
cloudfox <348868282@qq.com>
2020-05-13 17:27:12 +0800

    update

--- js/hexo-theme-yun.js
+++ js/hexo-theme-yun.js
@@ -1,9 +1,3 @@
-console.log(
-  "%c ☁️ hexo-theme-yun %c https://github.com/YunYouJun/hexo-theme-yun",
-  "color: white; background: #0078E7; padding:5px 0;",
-  "padding:4px;border:1px solid #0078E7;"
-);
-
 // scroll
 function scrollPercent(curTop) {
   const bodyHeight = document.body.clientHeight;
